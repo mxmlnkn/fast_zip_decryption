884e31dabbae48316d8de70ef9e07b8a22e6c9a0
kamilmahmood <kamil.mahmood@outlook.com>
2024-09-30 05:03:51 +0500

    reverted setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from setuptools import setup, Extension
+from setuptools import (
+        setup,
+        Extension
+    )
 from setuptools.command.build_ext import build_ext
+from os import path
 
+here = path.abspath(path.dirname(__file__))
 
 # https://github.com/cython/cython/blob/master/docs/src/tutorial/appendix.rst#python-38
 class Build(build_ext):
@@ -19,4 +24,32 @@ decrypter = Extension(
     sources=["extension/_zipdecryptermodule.c"],
     language="c"
 )
-setup(ext_modules=[decrypter], cmdclass={"build_ext": Build})
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fastzipfile',
+    version='v2.3.1',
+    description='Read password protected Zips 100x faster',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kamilmahmood/fastzipfile',
+    author='Kamil Mahmood',
+    author_email='kamil.mahmood@outlook.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='zip zipfile fastzip',
+    python_requires='>=3.8',
+    py_modules=['fastzipfile'],
+    ext_modules=[decrypter],
+    zip_safe=False,
+    cmdclass={"build_ext": Build}
+)
